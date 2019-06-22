76ab66fdc76e3b8f03b584a7df56c5827ddf7cd3
Leo Arias <elopio@zeppelin.solutions>
2019-06-22 01:35:41 +0000

    Prepare for when we can manage the hosted zone with terraform.
    According to AWS, it can take up to 48 hours to recover the zone.

--- aws/openzeppelin.org-dns.tf
+++ aws/openzeppelin.org-dns.tf
@@ -7,8 +7,40 @@ locals {
   netlify_ip = "104.198.14.52"
 }
 
+#resource "aws_route53_zone" "openzeppelin_org" {
+#  name = "openzeppelin.org"
+#}
+
+#resource "aws_route53_record" "nameservers" {
+#  allow_overwrite = true
+#  name            = "openzeppelin.org"
+#  ttl             = 30
+#  type            = "NS"
+#  zone_id         = "${aws_route53_zone.openzeppelin_org.zone_id}"
+#
+#  records = [
+#    "${aws_route53_zone.openzeppelin_org.name_servers.0}",
+#    "${aws_route53_zone.openzeppelin_org.name_servers.1}",
+#    "${aws_route53_zone.openzeppelin_org.name_servers.2}",
+#    "${aws_route53_zone.openzeppelin_org.name_servers.3}",
+#  ]
+#}
+
+#resource "aws_route53_record" "soa" {
+#  allow_overwrite = true
+#  name            = "openzeppelin.org"
+#  ttl             = 900
+#  type            = "SOA"
+#  zone_id         = "${aws_route53_zone.openzeppelin_org.zone_id}"
+#
+#  records = [
+#    "${aws_route53_zone.openzeppelin_org.name_servers.0}. awsdns-hostmaster.amazon.com. 1 7200 900 1209600 86400"
+#  ]
+#}
+
 resource "aws_route53_record" "website" {
   zone_id = "Z4OXHKVXYGOI"
+#  zone_id = "${aws_route53_zone.openzeppelin_org.zone_id}"
   name    = "openzeppelin.org"
   ttl     = "300"
   type    = "A"
@@ -17,6 +49,7 @@ resource "aws_route53_record" "website" {
 
 resource "aws_route53_record" "aws_certificate_validation" {
   zone_id = "Z4OXHKVXYGOI"
+#  zone_id = "${aws_route53_zone.openzeppelin_org.zone_id}"
   name    = "_df0a7bc1a38ac3b6105129ad125344ab.openzeppelin.org"
   ttl     = "300"
   type    = "CNAME"
@@ -25,6 +58,7 @@ resource "aws_route53_record" "aws_certificate_validation" {
 
 resource "aws_route53_record" "aws_certificate_validation_www" {
   zone_id = "Z4OXHKVXYGOI"
+#  zone_id = "${aws_route53_zone.openzeppelin_org.zone_id}"
   name    = "_7c9144567bcc1b51ee510d4338c4ab65.www.openzeppelin.org"
   ttl     = "300"
   type    = "CNAME"
@@ -33,6 +67,7 @@ resource "aws_route53_record" "aws_certificate_validation_www" {
 
 resource "aws_route53_record" "mailgun_mx" {
   zone_id = "Z4OXHKVXYGOI"
+#  zone_id = "${aws_route53_zone.openzeppelin_org.zone_id}"
   name    = "openzeppelin.org"
   ttl     = "300"
   type    = "MX"
@@ -44,6 +79,7 @@ resource "aws_route53_record" "mailgun_mx" {
 
 resource "aws_route53_record" "mailgun_txt" {
   zone_id = "Z4OXHKVXYGOI"
+#  zone_id = "${aws_route53_zone.openzeppelin_org.zone_id}"
   name    = "openzeppelin.org"
   ttl     = "300"
   type    = "TXT"
@@ -52,6 +88,7 @@ resource "aws_route53_record" "mailgun_txt" {
 
 resource "aws_route53_record" "mailgun_txt_k1" {
   zone_id = "Z4OXHKVXYGOI"
+#  zone_id = "${aws_route53_zone.openzeppelin_org.zone_id}"
   name    = "k1._domainkey.openzeppelin.org"
   ttl     = "300"
   type    = "TXT"
@@ -62,6 +99,7 @@ resource "aws_route53_record" "mailgun_txt_k1" {
 
 resource "aws_route53_record" "www" {
   zone_id = "Z4OXHKVXYGOI"
+#  zone_id = "${aws_route53_zone.openzeppelin_org.zone_id}"
   name    = "www.openzeppelin.org"
   ttl     = 300
   type    = "CNAME"
@@ -70,6 +108,7 @@ resource "aws_route53_record" "www" {
 
 resource "aws_route53_record" "docs" {
   zone_id = "Z4OXHKVXYGOI"
+#  zone_id = "${aws_route53_zone.openzeppelin_org.zone_id}"
   name    = "docs.openzeppelin.org"
   ttl     = 300
   type    = "CNAME"
@@ -78,6 +117,7 @@ resource "aws_route53_record" "docs" {
 
 resource "aws_route53_record" "forum" {
   zone_id = "Z4OXHKVXYGOI"
+#  zone_id = "${aws_route53_zone.openzeppelin_org.zone_id}"
   name    = "forum.openzeppelin.org"
   ttl     = 300
   type    = "CNAME"
@@ -86,6 +126,7 @@ resource "aws_route53_record" "forum" {
 
 resource "aws_route53_record" "slack_landing_page" {
   zone_id = "Z4OXHKVXYGOI"
+#  zone_id = "${aws_route53_zone.openzeppelin_org.zone_id}"
   name    = "slack.openzeppelin.org"
   ttl     = 300
   type    = "CNAME"

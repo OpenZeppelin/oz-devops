e489a570f2315cb45552e34aa4d3794cfe50f7de
Leo Arias <elopio@zeppelin.solutions>
2019-07-03 02:33:49 +0000

    Add zpl.in records

---
+++ aws/zpl.in-dns.tf
@@ -0,0 +1,51 @@
+#resource "aws_route53_zone" "zpl_in" {
+#  name = "zpl.in"
+#}
+
+#resource "aws_route53_record" "zpl_in_nameservers" {
+#  allow_overwrite = true
+#  name            = "zpl.in"
+#  ttl             = 172800
+#  type            = "NS"
+#  zone_id         = "${aws_route53_zone.zpl_in.zone_id}"
+#
+#  records = [
+#    "${aws_route53_zone.zpl_in.name_servers.0}",
+#    "${aws_route53_zone.zpl_in.name_servers.1}",
+#    "${aws_route53_zone.zpl_in.name_servers.2}",
+#    "${aws_route53_zone.zpl_in.name_servers.3}",
+#  ]
+#
+
+#resource "aws_route53_record" "zpl_in_soa" {
+#  allow_overwrite = true
+#  name            = "zpl.in"
+#  ttl             = 900
+#  type            = "SOA"
+#  zone_id         = "${aws_route53_zone.zpl_in.zone_id}"
+#
+#  records = [
+#    "${aws_route53_zone.zpl_in.name_servers.0}. awsdns-hostmaster.amazon.com. 1 7200 900 1209600 86400"
+#  ]
+#}
+
+resource "aws_route53_record" "zpl_in_website" {
+  zone_id = "Z3NTEMISW86T2R"
+#  zone_id = "${aws_route53_zone.zpl_in.zone_id}"
+  name    = "zpl.in"
+  type    = "A"
+  alias {
+    name                   = "d3vpyoihivlbza.cloudfront.net."
+    zone_id                = " Z2FDTNDATAQYW2"
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "zpl_in_ethernaut" {
+  zone_id = "Z3NTEMISW86T2R"
+#  zone_id = "${aws_route53_zone.zpl_in.zone_id}"
+  name    = "zpl.in"
+  ttl     = "300"
+  type    = "A"
+  records = ["54.86.213.93"]
+}

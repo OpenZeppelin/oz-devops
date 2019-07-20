cade8c6f57785dcc783bf87385e7b2cf8c3adedf
Leo Arias <elopio@zeppelin.solutions>
2019-07-20 21:21:03 +0000

    Add the record for crafty.openzeppelin.com

--- aws/openzeppelin.com-dns.tf
+++ aws/openzeppelin.com-dns.tf
@@ -87,3 +87,14 @@ resource "aws_route53_record" "openzeppelin_com_forum" {
     "zeppelin.hosted-by-discourse.com"
   ]
 }
+
+resource "aws_route53_record" "openzeppelin_com_crafty" {
+  zone_id = "${aws_route53_zone.openzeppelin_com.zone_id}"
+  name    = "crafty.openzeppelin.com"
+  type    = "A"
+  alias {
+    name                   = "d36f6jlxd1t7vv.cloudfront.net."
+    zone_id                = "Z2FDTNDATAQYW2"
+    evaluate_target_health = false
+  }
+}

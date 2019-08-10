552dbae02d0e46b79e6982dd8b1379af30db1961
Leo Arias <elopio@openzeppelin.com>
2019-08-10 15:45:52 +0000

    Fully manage the zpl.in domain

--- dns/zpl.in-dns.tf
+++ dns/zpl.in-dns.tf
@@ -1,37 +1,36 @@
-#resource "aws_route53_zone" "zpl_in" {
-#  name = "zpl.in"
-#}
+resource "aws_route53_zone" "zpl_in" {
+  name = "zpl.in"
+}
 
-#resource "aws_route53_record" "zpl_in_nameservers" {
-#  allow_overwrite = true
-#  name            = "zpl.in"
-#  ttl             = 172800
-#  type            = "NS"
-#  zone_id         = "${aws_route53_zone.zpl_in.zone_id}"
-#
-#  records = [
-#    "${aws_route53_zone.zpl_in.name_servers.0}",
-#    "${aws_route53_zone.zpl_in.name_servers.1}",
-#    "${aws_route53_zone.zpl_in.name_servers.2}",
-#    "${aws_route53_zone.zpl_in.name_servers.3}",
-#  ]
-#
+resource "aws_route53_record" "zpl_in_nameservers" {
+  allow_overwrite = true
+  name            = "zpl.in"
+  ttl             = 172800
+  type            = "NS"
+  zone_id         = "${aws_route53_zone.zpl_in.zone_id}"
 
-#resource "aws_route53_record" "zpl_in_soa" {
-#  allow_overwrite = true
-#  name            = "zpl.in"
-#  ttl             = 900
-#  type            = "SOA"
-#  zone_id         = "${aws_route53_zone.zpl_in.zone_id}"
-#
-#  records = [
-#    "${aws_route53_zone.zpl_in.name_servers.0}. awsdns-hostmaster.amazon.com. 1 7200 900 1209600 86400"
-#  ]
-#}
+  records = [
+    "${aws_route53_zone.zpl_in.name_servers.0}",
+    "${aws_route53_zone.zpl_in.name_servers.1}",
+    "${aws_route53_zone.zpl_in.name_servers.2}",
+    "${aws_route53_zone.zpl_in.name_servers.3}",
+  ]
+
+
+resource "aws_route53_record" "zpl_in_soa" {
+  allow_overwrite = true
+  name            = "zpl.in"
+  ttl             = 900
+  type            = "SOA"
+  zone_id         = "${aws_route53_zone.zpl_in.zone_id}"
+
+  records = [
+    "${aws_route53_zone.zpl_in.name_servers.0}. awsdns-hostmaster.amazon.com. 1 7200 900 1209600 86400"
+  ]
+}
 
 resource "aws_route53_record" "zpl_in_website" {
-  zone_id = "Z3NTEMISW86T2R"
-#  zone_id = "${aws_route53_zone.zpl_in.zone_id}"
+  zone_id = "${aws_route53_zone.zpl_in.zone_id}"
   name    = "zpl.in"
   type    = "A"
   alias {
@@ -42,8 +41,7 @@ resource "aws_route53_record" "zpl_in_website" {
 }
 
 resource "aws_route53_record" "zpl_in_ethernaut" {
-  zone_id = "Z3NTEMISW86T2R"
-#  zone_id = "${aws_route53_zone.zpl_in.zone_id}"
+  zone_id = "${aws_route53_zone.zpl_in.zone_id}"
   name    = "zpl.in"
   ttl     = "300"
   type    = "A"

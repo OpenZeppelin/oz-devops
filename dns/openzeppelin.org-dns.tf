d1374c205524ad3084e7751a91a489b522cd264b
Juan Bautista Carpanelli <juanbanelli@gmail.com>
2019-08-23 22:58:24 +0200

    Add xdai relayer

--- dns/openzeppelin.org-dns.tf
+++ dns/openzeppelin.org-dns.tf
@@ -175,3 +175,11 @@ resource "aws_route53_record" "openzeppelin_org_gns_ropsten_01" {
   type    = "A"
   records = ["75.101.212.80"]
 }
+
+resource "aws_route53_record" "openzeppelin_org_gns_xdai_01" {
+  zone_id = "${aws_route53_zone.openzeppelin_org.zone_id}"
+  name    = "xdai-01.gsn.openzeppelin.org"
+  ttl     = 300
+  type    = "A"
+  records = ["54.236.31.162"]
+}
